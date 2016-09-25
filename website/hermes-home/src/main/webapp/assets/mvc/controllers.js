1dea0f7fbb6b5343a92ed5aea8175d8a4686eb6a
Martin <bulong0721@163.com>
2016-09-25 21:04:03 +0800

    09-25晚

--- website/hermes-home/src/main/webapp/assets/mvc/controllers.js
+++ website/hermes-home/src/main/webapp/assets/mvc/controllers.js
@@ -6,12 +6,34 @@
 /**
  * MainCtrl - controller
  */
-function MainCtrl() {
+function MainCtrl($timeout) {
 
     this.shopName = '千途物流公司';
     this.userName = 'Martin.Xu';
     this.helloText = 'Welcome in SeedProject';
     this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
+
+    this.minimalize = function () {
+        $("body").toggleClass("mini-navbar");
+        if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+            // Hide menu in order to smoothly turn on when maximize menu
+            $('#side-menu').hide();
+            // For smoothly turn on menu
+            setTimeout(
+                function () {
+                    $('#side-menu').fadeIn(400);
+                }, 200);
+        } else if ($('body').hasClass('fixed-sidebar')) {
+            $('#side-menu').hide();
+            setTimeout(
+                function () {
+                    $('#side-menu').fadeIn(400);
+                }, 100);
+        } else {
+            // Remove all inline style from jquery fadeIn function to reset menu state
+            $('#side-menu').removeAttr('style');
+        }
+    }
 }
 
 angular
